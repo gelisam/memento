5d2af3a30515e93c5ceb069bbb8504852a0074bf
Samuel Gélineau <gelisam+github@gmail.com>
2017-12-27 21:59:24 -0500

    memento version of the simple-fltk demo

--- memento-simple-fltkhs/demo/Main.hs
+++ memento-simple-fltkhs/demo/Main.hs
@@ -1,18 +1,54 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module Main where
 
+import Data.Text
+
 import MementoSimpleFLTK
-import SimpleFLTK
 
 
-view :: () -> Button ()
-view () = Button (Rectangle (Position (X 0) (Y 0))
-                            (Size (Width 100) (Height 100)))
-                 "hello"
-                 ()
+data AppState = AppState
+  { appStateGoingUp :: Bool
+  , appStateCount   :: Int
+  }
+  deriving Show
 
-onEvent :: () -> () -> ()
-onEvent () () = ()
+initialState :: AppState
+initialState = AppState True 0
+
+
+data Event
+  = ToggleDirection
+  | BumpCount
+  deriving (Eq, Show)
+
+toggleDirection :: AppState -> AppState
+toggleDirection appState@AppState{..} = appState
+                                      { appStateGoingUp = not appStateGoingUp }
+
+bumpCount :: AppState -> AppState
+bumpCount appState@AppState{..} = appState
+                                { appStateCount = if appStateGoingUp
+                                                  then appStateCount + 1
+                                                  else appStateCount - 1
+                                }
+
+update :: Event -> AppState -> AppState
+update ToggleDirection = toggleDirection
+update BumpCount       = bumpCount
+
+
+windowSize :: Size
+windowSize = Size (Width 130) (Height 90)
+
+render :: AppState -> Pair (Button Event)
+render AppState{..} = Pair (Button (Rectangle (Position (X 30) (Y 30))
+                                              (Size (Width 30) (Height 30)))
+                                   (if appStateGoingUp then "^" else "v")
+                                   ToggleDirection)
+                           (Button (Rectangle (Position (X 70) (Y 30))
+                                              (Size (Width 30) (Height 30)))
+                                   (pack . show $ appStateCount)
+                                   BumpCount)
 
 main :: IO ()
-main = runMementoFLTK () view onEvent
+main = runMementoSimpleFLTK initialState update windowSize render
--- memento-simple-fltkhs/src/MementoSimpleFLTK.hs
+++ memento-simple-fltkhs/src/MementoSimpleFLTK.hs
@@ -1,7 +1,13 @@
-{-# LANGUAGE DeriveFunctor, FlexibleInstances, MultiParamTypeClasses, TypeFamilies #-}
+{-# LANGUAGE DeriveFunctor, FlexibleInstances, MultiParamTypeClasses, ScopedTypeVariables, TypeFamilies, UndecidableInstances #-}
 {-# OPTIONS -Wno-orphans #-}
-module MementoSimpleFLTK where
+module MementoSimpleFLTK
+ ( module MementoSimpleFLTK
+ , Rectangle(..), Position(..), X(..), Y(..), Size(..),Width(..), Height(..)
+ ) where
 
+import Control.Applicative
+import Control.Monad.IO.Class
+import Data.IORef
 import Data.Monoid
 import Data.Text
 
@@ -39,8 +45,8 @@ instance Diff patchEvent => Diff (PatchButton patchEvent) where
   diff (Button x1 x2 x3) (Button x1' x2' x3')
     = PatchButton (diff x1 x1') (diff x2 x2') (diff x3 x3')
 
---- Because of Memento's fundeps, this instance considered an orphan even though
---- PatchButton is defined in this file :(
+-- Because of Memento's fundeps, this instance is considered an orphan even
+-- though PatchButton is defined in this file :(
 instance Memento SimpleFLTK ButtonRef (PatchButton (Last Callback)) where
   newMemento (Button rect label callback) = newButton rect label callback
   modifyMemento buttonRef (PatchButton (Last maybeRectangle) (Last maybeLabel) (Last maybeCallback)) = do
@@ -51,9 +57,76 @@ instance Memento SimpleFLTK ButtonRef (PatchButton (Last Callback)) where
   deleteMemento = deleteButton
 
 
-runMementoFLTK :: (Action model, Eq event)
-               => model
-               -> (model -> Button event)
-               -> (event -> model -> model)
-               -> IO ()
-runMementoFLTK _ _ _ = pure ()
+-- Hack to allow two buttons.
+-- TODO: allow a variable number of buttons
+data Pair a = Pair a a
+  deriving (Functor, Show)
+
+instance Applicative Pair where
+  pure x = Pair x x
+  Pair f1 f2 <*> Pair x1 x2 = Pair (f1 x1) (f2 x2)
+
+instance Foldable Pair where
+  foldMap f (Pair x1 x2) = f x1 <> f x2
+
+instance Traversable Pair where
+  traverse f (Pair x1 x2) = Pair <$> f x1 <*> f x2
+
+instance Monoid a => Monoid (Pair a) where
+  mempty = pure mempty
+  mappend = liftA2 mappend
+
+instance Action a => Action (Pair a) where
+  type Operand (Pair a) = Pair (Operand a)
+  act = liftA2 act
+
+instance Diff a => Diff (Pair a) where
+  diff = liftA2 diff
+
+instance Memento m o a => Memento m (Pair o) (Pair a) where
+  newMemento = traverse newMemento
+  modifyMemento o a = sequenceA $ modifyMemento <$> o <*> a
+  deleteMemento = mapM_ deleteMemento
+
+overPair :: (a -> b) -> Pair a -> Pair b
+overPair f (Pair x1 x2) = Pair (f x1) (f x2)
+
+
+runMementoSimpleFLTK :: forall model event. Eq event
+                     => model
+                     -> (event -> model -> model)
+                     -> Size
+                     -> (model -> Pair (Button event))
+                     -> IO ()
+runMementoSimpleFLTK initialModel handler windowSize render = do
+  -- initialized below in runSimpleFLTK
+  ref <- newIORef (undefined :: (model, Pair (Button event), Pair (Button Callback), Pair ButtonRef))
+
+  let initialEventButtons :: Pair (Button event)
+      initialEventButtons = render initialModel
+
+      initialCallbackButtons :: Pair (Button Callback)
+      initialCallbackButtons = overPair toCallbackButton initialEventButtons
+
+      toCallbackButton :: Button event -> Button Callback
+      toCallbackButton = fmap update
+
+      toPatchCallbackButton :: PatchButton (Last event) -> PatchButton (Last Callback)
+      toPatchCallbackButton = (fmap . fmap) update
+
+      update :: event -> SimpleFLTK ()
+      update event = do
+        (model, eventButtons, callbackButtons, buttonRefs) <- liftIO $ readIORef ref
+        let model' = handler event model
+            eventButtons' = render model'
+            callbackButtons' = act patchCallbackButtons callbackButtons
+            patchCallbackButtons = overPair toPatchCallbackButton patchEventButtons
+            patchEventButtons = diff eventButtons eventButtons'
+        buttonRefs' <- modifyMemento buttonRefs patchCallbackButtons
+        liftIO $ writeIORef ref (model', eventButtons', callbackButtons', buttonRefs')
+
+  runSimpleFLTK windowSize $ do
+    initialButtonRefs <- newMemento initialCallbackButtons
+    liftIO $ writeIORef ref (initialModel, initialEventButtons, initialCallbackButtons, initialButtonRefs)
+
+  -- TODO: Add a finalizer to runSimpleFLTK so we can call deleteMemento at the end
